37678d3f1ef6889d23884e1338a0b62237697072
Nicolas <n.ico15935746@gmail.com>
2021-05-21 00:46:15 -0500

    [Added] Comments

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        ''' more comments'''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,14 +17,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        ''' this is not my code '''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        ''' i just want 80% '''
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
